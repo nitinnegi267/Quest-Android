e6b7f57107408863bfdcdaf521b23c42460f5bc1
nitinnegixplor <144686880+nitinnegixplor@users.noreply.github.com>
2025-01-12 18:12:15 +0530

    Added Room and Navigation Module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+        classpath(libs.navigation.safeArgs.generator)
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.navigation.safeargs.kotlin) apply false
 }
---
+++ navigationcomponentdemo/src/main/java/com/example/navigationcomponentdemo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.navigationcomponentdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.main_activity)
+
+
+    }
+}
+
+
+
